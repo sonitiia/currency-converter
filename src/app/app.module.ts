b45574c9db371345e90c2ee9d9c70986a2614bfb
sonitiia <sofiatymechko@gmail.com>
2023-06-02 15:16:54 +0300

    add angular material, rewrite components using default styled theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ConverseCurrencyFormComponent } from './forms/converse-currency-form/converse-currency-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, ConverseCurrencyFormComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/forms/converse-currency-form/converse-currency-form.component.ts
+++ src/app/forms/converse-currency-form/converse-currency-form.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import { ApiCurrencyService } from 'src/app/services/api-currency.service';
 
+interface CurrencyCode {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-converse-currency-form',
   templateUrl: './converse-currency-form.component.html',
   styleUrls: ['./converse-currency-form.component.css'],
 })
 export class ConverseCurrencyFormComponent {
+  currencyCodes: CurrencyCode[] = [
+    { value: 'UAH', viewValue: 'Ukrainian hryvnia' },
+    { value: 'USD', viewValue: 'U.S. dollar' },
+    { value: 'EUR', viewValue: 'Euro' },
+  ];
+
   fromInputValue: any;
   toInputValue: any;
   fromSelectValue: any;
   toSelectValue: any;
+
+  selectedFrom = 'USD';
+  selectedTo = 'UAH';
+
   constructor(private apiCurrencyService: ApiCurrencyService) {}
 
   convertFrom(
